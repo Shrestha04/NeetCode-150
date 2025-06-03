124dd6b604952f79b39860f3bd225c5e8e5f0c3b
Shrestha Jaiswal <shrestha.jaiswal04@gmail.com>
2025-06-03 20:41:04 +0530

    Update Valid_Palindrome.cpp

--- Two Pointers/Valid_Palindrome.cpp
+++ Two Pointers/Valid_Palindrome.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+        int start=0;
+        int end=s.length()-1;
 
+        while(start<end){
+            while(start<end && !alphaNum(s[start])){
+                start++;
+            }  
+            while(end>start && !alphaNum(s[end])){
+                end--;
+            }
+
+            if(tolower(s[start]) != tolower(s[end])){
+                return false;
+            }
+            
+            start++;
+            end--;
+        }
+        return true;   
+    }
+    bool alphaNum(char c){
+        return (c>='A' && c<='Z' || c>='a' && c<'z' || c>='0' && c<='9');
+    }
+};
