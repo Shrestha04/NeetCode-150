e5a14c880795d77c4e4e186f183441c07423c07d
Shrestha Jaiswal <shrestha.jaiswal04@gmail.com>
2025-06-05 23:55:39 +0530

    Update Top_K_Frequent_Elements.cpp

--- Arrays & Hashing/Top_K_Frequent_Elements.cpp
+++ Arrays & Hashing/Top_K_Frequent_Elements.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        for (int num : nums) {
+            mp[num]++;
+        }
 
+        vector<pair<int, int>> ans;
+        for (const auto& p : mp) {
+            ans.push_back({p.second, p.first});
+        }
+        sort(ans.rbegin(), ans.rend());
+
+        vector<int> res;
+        for (int i = 0; i < k; ++i) {
+            res.push_back(ans[i].second);
+        }
+        return res;
+    }
+};
