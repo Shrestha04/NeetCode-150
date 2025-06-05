108589a4cd0f614be1cce6223f3899d2b6da17aa
Shrestha Jaiswal <shrestha.jaiswal04@gmail.com>
2025-06-05 23:51:34 +0530

    Update Group_Anagrams.cpp

--- Arrays & Hashing/Group_Anagrams.cpp
+++ Arrays & Hashing/Group_Anagrams.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>>mp;
+
+        for(const auto& s:strs){
+            string ans=s;
+            sort(ans.begin(),ans.end());
+            mp[ans].push_back(s);
+        }
+
+        vector<vector<string>> result;
+        for(auto& str : mp){
+            result.push_back(str.second);
+        }
+        return result;
+    }
+};
 
