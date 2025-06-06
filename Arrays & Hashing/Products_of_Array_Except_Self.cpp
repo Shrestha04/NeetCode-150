3880fee0fd813a948d73bf7fc868072a9b73c785
Shrestha Jaiswal <shrestha.jaiswal04@gmail.com>
2025-06-06 21:31:26 +0530

    Update Products_of_Array_Except_Self.cpp

--- Arrays & Hashing/Products_of_Array_Except_Self.cpp
+++ Arrays & Hashing/Products_of_Array_Except_Self.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans(n);
+
+        for(int i=0;i<n;i++){
+            int prod=1;
+            for(int j=0;j<n;j++){
+                if(j!=i){
+                    prod*=nums[j];
+                }
+            }
+            ans[i]=prod;
+        }
+        return ans;
+    }
+};
 
