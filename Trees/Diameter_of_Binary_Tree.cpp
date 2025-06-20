1867e19956f57d17d24a226238819899d26ffe1c
Shrestha Jaiswal <shrestha.jaiswal04@gmail.com>
2025-06-20 15:16:28 +0530

    Update Diameter_of_Binary_Tree.cpp

--- Trees/Diameter_of_Binary_Tree.cpp
+++ Trees/Diameter_of_Binary_Tree.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        if( root == nullptr){
+            return 0;
+        }
 
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
+        int diameter = leftHeight + rightHeight;
+
+        int left = diameterOfBinaryTree(root->left);
+        int right = diameterOfBinaryTree(root->right);
+        int subtree = max(left, right);
+
+        return max(diameter, subtree);
+    }
+
+    int height(TreeNode* root) {
+        if (!root) return 0;
+        return 1 + max(height(root->left), height(root->right));   
+    }
+};
