79a07f010be462f31c5a79dbf44ab3b6bd673cdb
Shrestha Jaiswal <shrestha.jaiswal04@gmail.com>
2025-06-20 15:15:08 +0530

    Update Maximum_Depth_of_Binary_Tree.cpp

--- Trees/Maximum_Depth_of_Binary_Tree.cpp
+++ Trees/Maximum_Depth_of_Binary_Tree.cpp
@@ -1,1 +1,14 @@
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
 
+        int leftv = maxDepth(root->left);
+        int rightv = maxDepth(root->right);
+        int ans = max(leftv, rightv);
+
+        return 1 + ans;
+    }
+};
