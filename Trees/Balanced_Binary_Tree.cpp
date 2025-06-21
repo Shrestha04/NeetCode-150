0e9f5f9eb5918bcec966df43d70ca80ff1345b28
Shrestha Jaiswal <shrestha.jaiswal04@gmail.com>
2025-06-21 12:11:33 +0530

    Update Balanced_Binary_Tree.cpp

--- Trees/Balanced_Binary_Tree.cpp
+++ Trees/Balanced_Binary_Tree.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if (!root) return true;
+        int leftH = checkH(root->left);
+        int rightH = checkH(root->right);
 
+        int ans = abs(leftH - rightH);
+
+        if(ans > 1) return false;
+        return isBalanced(root->left) && isBalanced(root->right);   
+    }
+
+    int checkH(TreeNode* root){
+
+        if(root == nullptr){
+            return 0; 
+        }
+
+        int leftv = checkH(root->left);
+        int rightv = checkH(root->right);
+
+        return max(leftv,rightv) + 1;
+    }
+};
