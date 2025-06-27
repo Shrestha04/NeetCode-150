40df7099063095f03f80b374f552a973ce023469
Shrestha Jaiswal <shrestha.jaiswal04@gmail.com>
2025-06-27 10:26:23 +0530

    Update Count_Good_Nodes_in_Binary_Tree.cpp

--- Trees/Count_Good_Nodes_in_Binary_Tree.cpp
+++ Trees/Count_Good_Nodes_in_Binary_Tree.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    int goodNodes(TreeNode* root) {
+        return dfs(root, root->val);
+    }
 
+    int dfs(TreeNode* node, int maxVal){
+        if(!node) return 0;
+
+        int res = (node->val>=maxVal) ? 1: 0;
+
+        maxVal = max(maxVal, node->val);
+        res+=dfs(node->left,maxVal);
+        res+=dfs(node->right,maxVal);
+        return res;
+    }
+};
