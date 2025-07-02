6e7844f1c9fc142e59c65da793149279d815da5b
Shrestha Jaiswal <shrestha.jaiswal04@gmail.com>
2025-07-02 21:57:16 +0530

    Create Serialize_and_Deserialize_Binary_Tree.cpp

---
+++ Trees/Serialize_and_Deserialize_Binary_Tree.cpp
@@ -0,0 +1,50 @@
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        if (!root) return "N";
+        string res;
+        queue<TreeNode*> queue;
+        queue.push(root);
+
+        while (!queue.empty()) {
+            TreeNode* node = queue.front();
+            queue.pop();
+            if (!node) {
+                res += "N,";
+            } else {
+                res += to_string(node->val) + ",";
+                queue.push(node->left);
+                queue.push(node->right);
+            }
+        }
+        return res;
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        stringstream ss(data);
+        string val;
+        getline(ss, val, ',');
+        if (val == "N") return nullptr;
+        TreeNode* root = new TreeNode(stoi(val));
+        queue<TreeNode*> queue;
+        queue.push(root);
+
+        while (getline(ss, val, ',')) {
+            TreeNode* node = queue.front();
+            queue.pop();
+            if (val != "N") {
+                node->left = new TreeNode(stoi(val));
+                queue.push(node->left);
+            }
+            getline(ss, val, ',');
+            if (val != "N") {
+                node->right = new TreeNode(stoi(val));
+                queue.push(node->right);
+            }
+        }
+        return root;
+    }
+};
