5137bf35cfb3ffd2783f088613e3b2c2d2ff4cf3
Shrestha Jaiswal <shrestha.jaiswal04@gmail.com>
2025-07-12 15:17:20 +0530

    Update Islands_and_Treasure.cpp

--- Graphs/Islands_and_Treasure.cpp
+++ Graphs/Islands_and_Treasure.cpp
@@ -1,40 +1,45 @@
 class Solution {
 public:
+    int ROWS, COLS;
     vector<vector<int>> directions = {{1, 0}, {-1, 0}, 
                                       {0, 1}, {0, -1}};
-    int INF = 2147483647;
-    vector<vector<bool>> visit;
-    int ROWS, COLS;
+    int INF = INT_MAX;
 
-    int dfs(vector<vector<int>>& grid, int r, int c) {
-        if (r < 0 || c < 0 || r >= ROWS || 
-            c >= COLS || grid[r][c] == -1 || visit[r][c]) {
-            return INF;
-        }
-        if (grid[r][c] == 0) {
-            return 0;
-        }
+    int bfs(vector<vector<int>>& grid, int r, int c) {
+        queue<pair<int, int>> q;
+        q.push({r, c});
+        vector<vector<bool>> visit(ROWS, vector<bool>(COLS, false));
         visit[r][c] = true;
-        int res = INF;
-        for (auto& dir : directions) {
-            int cur = dfs(grid, r + dir[0], c + dir[1]);
-            if (cur != INF) {
-                res = min(res, 1 + cur);
+        int steps = 0;
+
+        while (!q.empty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                auto [row, col] = q.front();
+                q.pop();
+                if (grid[row][col] == 0) return steps;
+                for (auto& dir : directions) {
+                    int nr = row + dir[0], nc = col + dir[1];
+                    if (nr >= 0 && nr < ROWS && nc >= 0 && nc < COLS && 
+                        !visit[nr][nc] && grid[nr][nc] != -1) {
+                        visit[nr][nc] = true;
+                        q.push({nr, nc});
+                    }
+                }
             }
+            steps++;
         }
-        visit[r][c] = false;
-        return res;
+        return INF;
     }
 
     void islandsAndTreasure(vector<vector<int>>& grid) {
         ROWS = grid.size();
         COLS = grid[0].size();
-        visit.assign(ROWS, vector<bool>(COLS, false));
 
-        for (int r = 0; r < ROWS; ++r) {
-            for (int c = 0; c < COLS; ++c) {
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
                 if (grid[r][c] == INF) {
-                    grid[r][c] = dfs(grid, r, c);
+                    grid[r][c] = bfs(grid, r, c);
                 }
             }
         }
